4e640858cae7da30c039295f6f2b5aba164437e2
mahaveer <mahaveersharma0496@yahoo.com>
2017-07-01 19:48:27 +0530

    added comments

--- src/redux/action.js
+++ src/redux/action.js
@@ -1,5 +1,11 @@
 import axios from 'axios';
 
+/*
+submissionStart etc are action creator as the name says they CREATE actions.
+they're just simple function returning an object which type: 'whatEver'
+every action creator MUST return an object with TYPE.
+
+*/
 const submissionStart = () => ({
   type: 'SUBMISSION_START',
 });
@@ -13,6 +19,20 @@ const submissionFail = () => ({
   type: 'SUBMISSION_FAIL',
 });
 
+
+/*
+this is also action creator but is different that it doesnt return a type but
+dispatches other actions.
+here;s the basic flow.
+when this function is invoked, it dispatches submissionStart -
+this submissionStart goes into the reducer where action.type is check and based on that
+new state is defined.
+after start. we make an async call if the call was successfull we dispatch new action
+submissionSuccess- now this again goes into the reducer and based on its action.type
+new state is defined again.
+if fail we just dispatch submissionFail and new state is defined again
+
+*/
 export const submit = ({ firstName, lastName, age }) => (dispatch) => {
   dispatch(submissionStart());
   axios.post('http://localhost:3000/api', { firstName, lastName, age })
--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -6,6 +6,18 @@ const initialState = {
   submitting: false,
   data: '',
 };
+
+/*
+reducer is a simple functions takes an Object (state) gives new obejct ( state )
+depending on the TYPE of ACTION.
+when SUBMISSION_START it returns new state with submitting:false so that
+we can show `Loading` animation or something,
+when SUBMISSION_SUCCESS new state is returned with `data : action.data`
+SUBMISSION_FAIL is just like SUBMISSION_START
+
+Note: Most important part - state should never be mutated
+*/
+
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case 'SUBMISSION_START':
